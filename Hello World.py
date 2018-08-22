09851f769ffa4c7398f19f4c84c4f248968898ed
JennitaBonita <gjenni4@lsu.edu>
2018-08-21 19:41:05 -0600

    First Python Script :snake:

---
+++ Hello World.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Created on Tue Aug 21
+@author: Jennie Garcia
+License: CC-BY-SA 4.0
+Description: This is a temporary script file.
+"""
+
+# set variables
+world_adjective = "new"
+
+# print statement
+print "Hola {adj} World!".format(adj=world_adjective)
+
+
+
+
+
